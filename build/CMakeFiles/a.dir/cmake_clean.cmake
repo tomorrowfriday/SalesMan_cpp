ecd1f5c009332996f06afe0017038c0e9ee158a0
U-HK55\skravets <skravets@hk55.domain.holtec.com>
2021-10-18 00:01:49 +0300

    first commit

---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/main.cpp.o"
+  "CMakeFiles/a.dir/main.cpp.o.d"
+  "a.exe"
+  "a.pdb"
+  "liba.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
